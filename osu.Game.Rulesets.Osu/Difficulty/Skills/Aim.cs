6f0c0bd36031077cd1f270c6398a323356805876
kwotaq <PJXORISJ@gmail.com>
2026-04-23 14:28:34 +0300

    attempt at less death

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -34,7 +34,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double skillMultiplierSnap => 70.9;
         private double skillMultiplierAgility => 2.35;
-        private double skillMultiplierFlow => 243.0;
+        private double skillMultiplierFlow => 260.0;
         private double skillMultiplierTotal => 0.78;
         private double combinedSnapNormExponent => 1.2;
 
@@ -209,10 +209,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
                 time += strain.SectionLength;
             }
 
-            return bonus * 0.107;
+            return bonus * 0.175;
         }
 
         // https://www.desmos.com/calculator/secrjaywao
-        public static double LengthBonusFormula(double ms) => Math.Pow(ms / 1000, 0.4);
+        public static double LengthBonusFormula(double ms) => Math.Pow(ms / 1000, 0.3);
     }
 }
