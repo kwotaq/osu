6d818a8670f5a98553be84025f3c16afb0d6e3fd
kwotaq <PJXORISJ@gmail.com>
2026-08-14 15:21:48 +0300

    scrap perfect stacks and let rhythm handle that

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -235,15 +235,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
                 loopDifficulty *= timeNerfFactor;
 
-                // Apply repetition and visibility nerfs only if
-                // the overlapping note has had movement between it and the current one or the rhythms are the same
-                if (Math.Max(currObj.AdjustedDeltaTime, loopObj.AdjustedDeltaTime) < 1.1 * Math.Min(currObj.AdjustedDeltaTime, loopObj.AdjustedDeltaTime) || notStacked)
-                {
-                    loopDifficulty *= distanceChangeFactor;
+                loopDifficulty *= distanceChangeFactor;
 
-                    // Greatly reduce difficulty depending on the visibility of the overlapping object
-                    loopDifficulty *= Math.Pow(currObj.OpacityAt(loopObj.BaseObject.StartTime, hidden), 2);
-                }
+                // Greatly reduce difficulty depending on the visibility of the overlapping object
+                loopDifficulty *= Math.Pow(currObj.OpacityAt(loopObj.BaseObject.StartTime, hidden), 2);
 
                 totalOverlapDifficulty += loopDifficulty;
             }
